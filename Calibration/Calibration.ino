b28f7e9a5b86aedb9abbf09501e925be8f976efe
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2024-12-10 23:39:33 +0200

    Change Calibration file

--- Calibration/Calibration.ino
+++ Calibration/Calibration.ino
@@ -1,20 +1,24 @@
 //Inputs outputs
-#define throttle_in 2
+#define throttle_in 33
 #define yaw_in 4
 #define pitch_in 34
 #define roll_in 39
 
 #define pot1_in 35
-#define pot2_in 15
+#define pot2_in 25
+
 #define toggle_1 32
-#define toggle_2 12
+#define toggle_2 26
+#define toggle_3 14
+#define toggle_4 27
+
 #define switch_1 22
 #define switch_2 36
 
-#define battery_in 33
-#define buttons_analog_in 27
-#define buzzer 12
-#define led 0
+#define battery_in 13
+#define buttons_analog_in 12
+#define buzzer 15
+#define led 2
 
 
 void setup() {
@@ -28,6 +32,8 @@ void setup() {
   pinMode(pot2_in, INPUT);
   pinMode(toggle_1, INPUT_PULLUP);
   pinMode(toggle_2, INPUT_PULLUP);
+  pinMode(toggle_3, INPUT_PULLUP);
+  pinMode(toggle_4, INPUT_PULLUP);
   pinMode(switch_1, INPUT_PULLUP);
   pinMode(switch_2, INPUT_PULLUP);
 
@@ -47,17 +53,23 @@ void loop() {
   int pot2 = analogRead(pot2_in);
   int toggle1 = digitalRead(toggle_1);
   int toggle2 = digitalRead(toggle_2);
+  int toggle3 = digitalRead(toggle_3);
+  int toggle4 = digitalRead(toggle_4);
   int switch1 = digitalRead(switch_1);
   int switch2 = digitalRead(switch_2);
+  int battery = analogRead(battery_in);
 
   Serial.print(" Throttle= "), Serial.print(throttle);
   Serial.print(" Yaw= "), Serial.print(yaw);
   Serial.print(" Pitch= "), Serial.print(pitch);
   Serial.print(" Roll= "), Serial.print(roll);
-  Serial.print(" AUX1= "), Serial.print(toggle1);
-  Serial.print(" AUX2= "), Serial.print(toggle2);
   Serial.print(" Pot1= "), Serial.print(pot1);
   Serial.print(" Pot2= "), Serial.print(pot2);
+  Serial.print(" AUX1= "), Serial.print(toggle1);
+  Serial.print(" AUX2= "), Serial.print(toggle2);
+  Serial.print(" AUX3= "), Serial.print(toggle3);
+  Serial.print(" AUX4= "), Serial.print(toggle4);
   Serial.print(" Switch1= "), Serial.print(switch1);
-  Serial.print(" Switch2= "), Serial.println(switch2);
+  Serial.print(" Switch2= "), Serial.print(switch2);
+  Serial.print(" Battery= "), Serial.println(battery);
 }
